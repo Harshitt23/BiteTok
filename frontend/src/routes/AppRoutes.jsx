7f405277ae4a1bcb066069e9449f403723201089
Harshitt23 <hasrhitr2308@gmail.com>
2025-09-28 14:31:16 +0530

    Started Working on Frontend

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -6,7 +6,7 @@ const AppRoutes = () => {
         <Router>
         <Routes>
             <Route path="/user/register" element={<h1>User Register</h1>} />
-            <Route path="/user/login" element={<h1>UserLogin</h1> } />
+            <Route path="/user/login" element={<h1>User Login</h1> } />
             <Route path="/food-partner/register" element={<h1>Food Partner Register</h1>} />
             <Route path="/food-partner/login" element={<h1>Food Partner Login</h1> }/>
             <Route path="*" element={<h1>Route not found</h1>} />
