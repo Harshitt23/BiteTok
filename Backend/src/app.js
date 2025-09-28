80b5508829aebe7dbfbfa124bb739c4ba6ee4808
Harshitt23 <hasrhitr2308@gmail.com>
2025-09-28 14:29:24 +0530

    intialized frontend and React router Setup done

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,6 +6,7 @@ const foodRoutes = require('./routes/food.routes');                           //
 
 
 const app = express();
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));                               //by -- cursor
@@ -13,6 +14,7 @@ app.use(express.urlencoded({ extended: true }));                               /
 app.get("/", (req, res) => {
     res.send("Hello bro keep going");
 })
+
 
 app.use('/api/auth', authRoutes);
 app.use('/api/food', foodRoutes);                                             //by -- cursor
