8ced98192b36d06c551c52e5c710fce0604bc5f4
Harshitt23 <hasrhitr2308@gmail.com>
2025-10-02 15:32:29 +0530

    back to working state

---
+++ api.js
@@ -0,0 +1,4 @@
+// Root level API file
+module.exports = (req, res) => {
+    res.status(200).json({ message: "ROOT API WORKS!" });
+};
---
+++ api/hello.js
@@ -0,0 +1,7 @@
+module.exports = (req, res) => {
+  res.status(200).json({ 
+    message: 'Hello from Vercel!',
+    timestamp: new Date().toISOString(),
+    working: true
+  });
+};
